762c482cb92021efa7219a74438c83f7719fb407
PrasanKumar <prasankumar93@gmail.com>
2024-09-25 12:34:18 +0530

    Dockerized frontend application

--- frontend/src/app/config.ts
+++ frontend/src/app/config.ts
@@ -1,8 +1,11 @@
 const config = {
-  API_BASE_URL: "http://localhost:3001/api",
-  SOCKET_IO_URL: "http://localhost:3001/",
-  DEFAULT_REDIS_URL: "redis://localhost:6379",
-  GIT_TAG: "v0.1.0",
+  API_BASE_URL:
+    process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3001/api",
+  SOCKET_IO_URL:
+    process.env.NEXT_PUBLIC_SOCKET_IO_URL || "http://localhost:3001/",
+  DEFAULT_REDIS_URL:
+    process.env.NEXT_PUBLIC_DEFAULT_REDIS_URL || "redis://localhost:6379",
+  GIT_TAG: "v0.2.0",
 };
 
 export { config };
